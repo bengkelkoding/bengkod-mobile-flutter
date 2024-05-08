4788735ead7e07a807ed31775d538c125b66b4ea
Muhamad Fannan <fananfalahi46@gmail.com>
2024-05-09 00:18:20 +0700

    update color

--- lib/utils/app_colors_palette.dart
+++ lib/utils/app_colors_palette.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static Color primaryColor = Color(0xFF114D91);
-  static Color secondPrimaryColor = Color(0xFFE45E5E);
+  static Color secondPrimaryColor = Color(0xFF03026B);
+  static Color thirdPrimaryColor = Color(0xFFE45E5E);
   static Color greenColor = Color(0xFF389517);
   static Color redColor = Color(0xFFDB2626);
   static Color whiteColor = Colors.white;
