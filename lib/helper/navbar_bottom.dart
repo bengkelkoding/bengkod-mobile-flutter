587c9a8bdf33a1b21869847d88d1425c436dbeb7
Shaka Arisya <shaka99rz@gmail.com>
2024-05-09 13:02:14 +0700

    update navbar_bottom

--- lib/helper/navbar_bottom.dart
+++ lib/helper/navbar_bottom.dart
@@ -4,41 +4,45 @@ import 'package:flutter_svg/svg.dart';
 class NavbarBottom extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(25),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: Offset(0, 5),
-          ),
-        ],
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(27),
+        topRight: Radius.circular(27),
       ),
-      child: BottomNavigationBar(
-        selectedItemColor:
-            Theme.of(context).primaryColor, // Warna primer untuk item terpilih
-        unselectedItemColor:
-            Colors.black, // Warna hitam untuk item yang tidak terpilih
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/icon/icon_home.svg'),
-            label: 'Beranda',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/icon/icon_classroom.svg'),
-            label: 'Kelas',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/icon/icon_assigment.svg'),
-            label: 'Tugas',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/icon/icon_profile.svg'),
-            label: 'Profile',
-          ),
-        ],
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.9), // Opasitas warna bayangan
+              offset: Offset(0, -4),
+              blurRadius: 10, // Tingkat kabur bayangan
+              spreadRadius: 2, // Menentukan seberapa jauh bayangan menyebar
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed, // Tambahkan ini
+          selectedItemColor: Theme.of(context).primaryColor,
+          unselectedItemColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icon/icon_home_hover.svg'),
+              label: 'Beranda',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icon/icon_classroom.svg'),
+              label: 'Kelas',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icon/icon_assigment.svg'),
+              label: 'Tugas',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset('assets/icon/icon_profile.svg'),
+              label: 'Profile',
+            ),
+          ],
+        ),
       ),
     );
   }
