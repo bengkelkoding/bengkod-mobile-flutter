a2f2a7eee184d3b81b83384da15e533dc8fb7890
Shaka Arisya <shaka99rz@gmail.com>
2024-05-17 22:03:41 +0700

    update config api http

---
+++ lib/config/api.dart
@@ -0,0 +1,7 @@
+class Api{
+  static const login = 'https://loved-dove-national.ngrok-free.app/api/v1/auth/login';
+  static const userProfile = 'https://loved-dove-national.ngrok-free.app/api/v1/profile';
+  static const refreshToken = 'https://loved-dove-national.ngrok-free.app/api/v1/auth/refresh-token';
+  static const allCourses = 'https://loved-dove-national.ngrok-free.app/api/v1/mobile/student/dashboard/courses';
+  static const latestAssignment = 'https://loved-dove-national.ngrok-free.app//api/v1/mobile/student/dashboard/latest-assignment';
+}
